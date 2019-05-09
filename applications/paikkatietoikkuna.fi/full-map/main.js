7c9ab5bba303fcc9bc781c7b9fe4854238ab2d9f
Sami Mäkinen <sami.makinen@nls.fi>
2019-05-09 16:07:58 +0300

    Take out guided tour for now

--- applications/paikkatietoikkuna.fi/full-map/main.js
+++ applications/paikkatietoikkuna.fi/full-map/main.js
@@ -33,7 +33,9 @@ import 'oskari-loader!oskari-frontend/packages/framework/bundle/maplegend/bundle
 import 'oskari-loader!oskari-frontend/packages/framework/bundle/userguide/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/catalogue/bundle/metadataflyout/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/framework/featuredata2/bundle.js';
-import 'oskari-loader!oskari-frontend/packages/framework/bundle/guidedtour/bundle.js';
+// commented out so we don't have 2 popups for the user to close 
+// resume after 10v quiz is taken out
+//import 'oskari-loader!oskari-frontend/packages/framework/bundle/guidedtour/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/framework/bundle/backendstatus/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/framework/bundle/printout/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/framework/bundle/postprocessor/bundle.js';
