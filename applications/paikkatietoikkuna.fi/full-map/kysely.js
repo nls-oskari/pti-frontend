25165c6669b089a6e691cc0e401c0d91a63fe28e
Sami Mäkinen <sami.makinen@nls.fi>
2019-05-14 11:58:12 +0300

    Full width banner

--- applications/paikkatietoikkuna.fi/full-map/kysely.js
+++ applications/paikkatietoikkuna.fi/full-map/kysely.js
@@ -4,8 +4,8 @@ import toastr from 'toastr';
 
 toastr.options.timeOut = 0;
 toastr.options.extendedTimeOut = 0;
-//toastr.options.positionClass = 'toast-top-full-width';
-toastr.options.positionClass = 'toast-top-center';
+toastr.options.positionClass = 'toast-top-full-width';
+//toastr.options.positionClass = 'toast-top-center';
 toastr.options.closeButton = true;
 
 window.toastr = toastr;
