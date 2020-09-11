512d34e3ccf9b957d8829c39d02617d8d46d749e
summamutikka <31657333+summamutikka@users.noreply.github.com>
2020-09-11 16:32:43 +0300

    Update fi.js

--- bundles/paikkatietoikkuna/inspire/resources/locale/fi.js
+++ bundles/paikkatietoikkuna/inspire/resources/locale/fi.js
@@ -4,13 +4,18 @@ Suomea koskevia paikkatietoaineistoja ja -palveluita esittelevä paikkatietopor
 
 INSPIREn tavoitteena on luoda 
 eurooppalainen paikkatietoinfrastruktuuri, joka helpottaa paikkatietojen saatavuutta kaikkialla Euroopassa ja edistää niiden 
-yhteentoimivuutta. Lue lisää sivulta <a target="_blank" href="https://www.maanmittauslaitos.fi/kartat-ja-paikkatieto/paikkatietojen-yhteentoimivuus/inspire/mika-inspire">Mikä INSPIRE?</a><br><br>
+yhteentoimivuutta. Lue lisää sivulta 
+<a target="_blank" href="https://www.maanmittauslaitos.fi/kartat-ja-paikkatieto/paikkatietojen-yhteentoimivuus/inspire/mika-inspire">Mikä INSPIRE?</a><br>
 
-Seuraa INSPIRE-somekanavia, niin saat nopeasti tietoa ajankohtaisista koulutuksista ja tapahtumista sekä Paikkatietoikkunaan liittyvistä asioista:<br>
-Paikkatietoikkuna <a target="_blank" href="https://twitter.com/geoportal_fi">Twitter</a><br>
-Paikkatietoikkuna <a target="_blank" href="https://fi-fi.facebook.com/paikkatietoikkuna/">Facebook</a><br><br>Tilaa 
+<br>Seuraa INSPIRE-somekanavia, niin saat nopeasti tietoa ajankohtaisista koulutuksista ja tapahtumista sekä Paikkatietoikkunaan liittyvistä asioista: 
+Paikkatietoikkunan <a target="_blank" href="https://twitter.com/geoportal_fi">Twitter</a> 
+ja <a target="_blank" href="https://fi-fi.facebook.com/paikkatietoikkuna/">Facebook</a><br>
 
-<a target="_blank" href="http://maanmittauslaitos.mailpv.net/">Paikkatietoverkoston uutiskirje</a>, josta saat tiiviissä muodossa tietoa paikkatietoalan ajankohtaisista asioista. Liityt samalla Paikkatietoverkostoon, jossa pääset mukaan vaikuttamaan ja keskustelemaan INSPIRE-toimeenpanoon ja kansalliseen paikkatietoinfrastruktuuriin liittyvistä asioista. Verkosto on avoin foorumi kaikille paikkatietoasioista kiinnostuneille. Joukossamme on jo yli 400 jäsentä! Aiemmin ilmestyneet uutiskirjeet löytyvät Paikkatietoalan verkostot -sivulta.
+<br>Tilaa <a target="_blank" href="http://maanmittauslaitos.mailpv.net/">Paikkatietoverkoston uutiskirje</a>, 
+josta saat tiiviissä muodossa tietoa paikkatietoalan ajankohtaisista asioista. Liityt samalla Paikkatietoverkostoon, 
+jossa pääset mukaan vaikuttamaan ja keskustelemaan INSPIRE-toimeenpanoon ja kansalliseen paikkatietoinfrastruktuuriin liittyvistä asioista. 
+Verkosto on avoin foorumi kaikille paikkatietoasioista kiinnostuneille. Joukossamme on jo yli 400 jäsentä! Aiemmin ilmestyneet uutiskirjeet 
+löytyvät Paikkatietoalan verkostot -sivulta.
 `;
 
 Oskari.registerLocalization({
