d80ac15df43aa06cc7ffdb6cf263319026284193
summamutikka <31657333+summamutikka@users.noreply.github.com>
2020-09-11 16:46:01 +0300

    Update en.js

--- bundles/paikkatietoikkuna/inspire/resources/locale/en.js
+++ bundles/paikkatietoikkuna/inspire/resources/locale/en.js
@@ -6,9 +6,7 @@ The objective of INSPIRE is to create a European spatial data infrastructure, wh
 everywhere in Europe while promoting their interoperability. Read more: <a href="https://inspire.ec.europa.eu/" target="_blank">What is INSPIRE?</a><br><br>
 
 Follow our INSPIRE social media channels (in Finnish) to get information on workshops and events as well as things related to Paikkatietoikkuna 
-geoportal:<br>
-Paikkatietoikkuna <a target="_blank" href="https://twitter.com/geoportal_fi">Twitter</a><br>
-Paikkatietoikkuna <a target="_blank" href="https://fi-fi.facebook.com/paikkatietoikkuna/">Facebook</a><br><br>
+geoportal:Paikkatietoikkuna <a target="_blank" href="https://twitter.com/geoportal_fi">Twitter</a><br> and <a target="_blank" href="https://fi-fi.facebook.com/paikkatietoikkuna/">Facebook</a><br><br>
 
 Subscribe to the newsletter of Paikkatietoverkosto, “Spatial data network” (in Finnish only), to get up-to-date information on what is happening 
 in the field of geoinformatics. By subscribing, you join the Spatial data network and get to discuss and share your view on issues related to 
