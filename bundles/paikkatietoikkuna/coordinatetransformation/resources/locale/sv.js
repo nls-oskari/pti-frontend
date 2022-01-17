f2a235ff3d90d05b306187c54448c752b61daae7
okauppinen <olli.kauppinen@nls.fi>
2022-01-17 11:22:11 +0200

    add localization

--- bundles/paikkatietoikkuna/coordinatetransformation/resources/locale/sv.js
+++ bundles/paikkatietoikkuna/coordinatetransformation/resources/locale/sv.js
@@ -47,7 +47,8 @@ Oskari.registerLocalization(
                 },
                 "heightSystem":{
                     "label": "Höjdsystem",
-                    "none": "Ingenting"
+                    "none": "Ingenting",
+                    "n43": "Observera, att deltanätet N43 inte täcker hela Norra Finland, och transformationen kan p.g.a detta eventuellt inte utföras. Tilläggsinformation (endast på finska, se Liite 2, bild 3) på sidan https://www.suomidigi.fi/ohjeet-ja-tuki/jhs-suositukset/jhs-163-suomen-korkeusjarjestelma-n2000."
                 }
             },
             "coordinateTable": {
