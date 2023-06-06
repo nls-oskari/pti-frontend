23b07ab20bc5302538ca4df0b30532d646ffa37b
Sami Mäkinen <sami.makinen@nls.fi>
2023-06-06 12:01:37 +0300

    Change inspire info -> service info

--- bundles/paikkatietoikkuna/inspire/resources/locale/fi.js
+++ bundles/paikkatietoikkuna/inspire/resources/locale/fi.js
@@ -1,12 +1,21 @@
 const flyoutTextContent = `
-<a target="_blank" href="https://www.maanmittauslaitos.fi/asioi-verkossa/paikkatietoikkuna">Paikkatietoikkuna</a> on keskeinen osa Suomen INSPIRE-toimeenpanoa ja kansallisten aineistojen näyteikkuna. Kannustamme kaikkia tiedontuottajia tuomaan aineistojaan ja palveluitaan saataville Paikkatietoikkunaan sekä kertomaan muutoksista rajapintapalveluissaan.<br>
-
-<br>INSPIREn tavoitteena on luoda eurooppalainen paikkatietoinfrastruktuuri, joka helpottaa paikkatietojen saatavuutta kaikkialla Euroopassa ja edistää niiden yhteentoimivuutta. Lue lisää sivulta 
-<a target="_blank" href="https://www.maanmittauslaitos.fi/kartat-ja-paikkatieto/paikkatietojen-yhteentoimivuus/inspire/mika-inspire">Mikä INSPIRE?</a><br>
-
-<br>Seuraa INSPIRE-somekanavia, niin saat nopeasti tietoa ajankohtaisista koulutuksista ja tapahtumista sekä Paikkatietoikkunaan liittyvistä asioista: 
-Paikkatietoikkunan <a target="_blank" href="https://twitter.com/geoportal_fi">Twitter</a> 
-ja <a target="_blank" href="https://fi-fi.facebook.com/paikkatietoikkuna/">Facebook</a>
+<p>
+    <a target="_blank" href="https://www.maanmittauslaitos.fi/asioi-verkossa/paikkatietoikkuna">Paikkatietoikkuna</a> on näyteikkuna suomalaisten organisaatioiden julkaisemiin karttatasoihin.
+    Organisaatiot tarjoavat niitä liitettäviksi Paikkatietoikkunaan. Maanmittauslaitos tekee tiedontuottajaorganisaatioiden kanssa sopimukset ennen kuin lisää karttatasot Paikkatietoikkunaan.
+</p>
+<p>
+Paikkatietoikkunan karttatasot pohjautuvat tiedontuottajien julkaisemiin rajapintapalveluihin, joten ne ovat yhtä ajantasaisia kuin tiedontuottajien omissa palveluissa.
+Karttatasojen sisältö ja ajantasaisuus ovat tiedontuottajien vastuulla.
+</p>
+<p>
+Paikkatietoikkuna on osa INSPIRE-direktiivin toimeenpanoa Suomessa. Direktiivi tähtää siihen, että eurooppalaiset paikkatiedot olisivat helposti saatavilla ja hyödynnettävissä.
+Lue lisää sivulta <a target="_blank" href="https://www.maanmittauslaitos.fi/kartat-ja-paikkatieto/paikkatietojen-yhteentoimivuus/inspire/mika-inspire">Mikä INSPIRE?</a></p>
+<p>
+Seuraa Paikkatietoikkunan somekanavia
+ <a target="_blank" href="https://twitter.com/geoportal_fi">Twitter</a> ja
+ <a target="_blank" href="https://fi-fi.facebook.com/paikkatietoikkuna/">Facebook</a>,
+ niin saat nopeasti tietoa Paikkatietoikkunaan ja INSPIREen liittyvistä asioista, ajankohtaisista koulutuksista ja tapahtumista. 
+</p>
 `;
 
 Oskari.registerLocalization({
@@ -14,10 +23,10 @@ Oskari.registerLocalization({
     'key': 'inspire',
     'value': {
         'tile': {
-            'title': 'INSPIRE'
+            'title': 'Tietoa palvelusta'
         },
         'flyout': {
-            'title': 'INSPIRE tuo paikkatiedot kaikkien saataville'
+            'title': 'Tietoa palvelusta'
         },
         'flyoutContent': {
             'content': flyoutTextContent
