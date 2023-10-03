748b610f0f71bbb985b4db306be4f1c3e9bfd0f1
Sami Mäkinen <sami.makinen@nls.fi>
2023-10-03 16:26:20 +0300

    Replace metadata search and feature data with new impls

--- applications/embedded-3D/main.js
+++ applications/embedded-3D/main.js
@@ -29,7 +29,7 @@ import 'oskari-loader!oskari-frontend/packages/framework/bundle/postprocessor/bu
 import 'oskari-loader!oskari-frontend/packages/framework/bundle/rpc/bundle.js';
 
 import 'oskari-lazy-loader?maplegend!oskari-frontend/packages/framework/bundle/maplegend/bundle.js';
-import 'oskari-lazy-loader?featuredata2!oskari-frontend/packages/framework/featuredata2/bundle.js';
+import 'oskari-lazy-loader?featuredata!oskari-frontend/packages/framework/featuredata/bundle.js';
 import 'oskari-lazy-loader?routingService!oskari-frontend/packages/framework/bundle/routingService/bundle.js';
 import 'oskari-lazy-loader?feedbackService!oskari-frontend/packages/framework/bundle/feedbackService/bundle.js';
 import 'oskari-lazy-loader?statsgrid!oskari-frontend/packages/statistics/statsgrid/bundle.js';
