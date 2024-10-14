48c8a2387f491dff2c1cd1610659567de4a61fd6
Olli Kauppinen <olli.kauppinen@nls.fi>
2024-10-14 21:43:37 +0300

    use new metadata flyout impl

--- applications/geoportal-3D/main.js
+++ applications/geoportal-3D/main.js
@@ -56,7 +56,7 @@ import 'oskari-lazy-loader?coordinatetransformation!../../packages/paikkatietoik
 import 'oskari-lazy-loader?statsgrid!oskari-frontend/bundles/statistics/statsgrid/bundle.js';
 import 'oskari-lazy-loader?terrain-profile!oskari-frontend-contrib/packages/terrain-profile/bundle.js';
 import 'oskari-lazy-loader?featuredata!oskari-frontend/packages/framework/featuredata/bundle.js';
-import 'oskari-lazy-loader?metadataflyout!oskari-frontend/packages/catalogue/bundle/metadataflyout/bundle.js';
+import 'oskari-lazy-loader?metadataflyout!oskari-frontend/bundles/catalogue/metadata/bundle.js'
 import 'oskari-lazy-loader?metadatasearch!oskari-frontend/packages/catalogue/metadatasearch/bundle.js';
 // added for mobile
 import 'oskari-lazy-loader?mobileuserguide!../../bundles/paikkatietoikkuna/mobileuserguide/bundle.js';
