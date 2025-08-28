91d3a2ce827ad9751d667ff921b6574c449b7d00
Olli Kauppinen <olli.kauppinen@nls.fi>
2025-08-28 16:29:35 +0300

    add optional onChange

--- bundles/paikkatietoikkuna/coordinatetransformation/view/ConfirmPopup.jsx
+++ bundles/paikkatietoikkuna/coordinatetransformation/view/ConfirmPopup.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { showPopup } from 'oskari-ui/components/window';
-import { Message } from 'oskari-ui';
+import { Message, Button, Tooltip } from 'oskari-ui';
 import { ButtonContainer, PrimaryButton, SecondaryButton } from 'oskari-ui/components/buttons';
 import { BUNDLE } from '../constants';
 
@@ -13,13 +13,21 @@ const Content = styled.div`
     padding: 20px;
 `;
 
-export const showConfirmPopup = (title, msg, onConfirm, onClose) => {
+export const showConfirmPopup = (title, msg, onConfirm, onClose, onChange) => {
+    const change = typeof onChange === 'function';
     return showPopup(
         <Message messageKey={title} bundleKey={BUNDLE} />,
         (<Content>
             <Message messageKey={msg} bundleKey={BUNDLE} />
             <ButtonContainer>
                 <SecondaryButton type='cancel' onClick={() => onClose()} />
+                { change &&
+                    <Tooltip title={<Message messageKey='confirm.changeTooltip' bundleKey={BUNDLE} />}>
+                        <Button className='t_change' onClick={() => onChange()}>
+                            <Message messageKey='confirm.change' bundleKey={BUNDLE}/>
+                        </Button>
+                    </Tooltip>
+                }
                 <PrimaryButton type='yes' onClick={() => onConfirm()}/>
             </ButtonContainer>
         </Content>),
