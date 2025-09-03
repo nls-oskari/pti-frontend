4e40527af652732efe79bf1a31b45de57c81878c
Olli Kauppinen <olli.kauppinen@nls.fi>
2025-09-03 14:08:59 +0300

    add buttons

--- bundles/paikkatietoikkuna/coordinatetransformation/view/InfoPopup.jsx
+++ bundles/paikkatietoikkuna/coordinatetransformation/view/InfoPopup.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { showPopup } from 'oskari-ui/components/window';
+import { ButtonContainer, PrimaryButton, SecondaryButton } from 'oskari-ui/components/buttons';
 import { BUNDLE } from '../constants';
 
 const POPUP_OPTIONS = {
@@ -24,17 +25,30 @@ const List = ({ items }) => {
         </ul>
     )
 };
-const getContent = (paragraphs, listItems) => (
-    <Content>
-        {paragraphs.map((p,i) => <Paragraph key={`p_${i}`}>{p}</Paragraph>)}
-        <List items={listItems} />
-    </Content>
-);
+const getContent = (paragraphs, listItems, onClose, onConfirm) => {
+    const renderButtons = typeof onClose === 'function' && typeof onConfirm === 'function';
+    const confirm = () => {
+        onConfirm();
+        onClose();
+    };
+    return(
+        <Content>
+            {paragraphs.map((p,i) => <Paragraph key={`p_${i}`}>{p}</Paragraph>)}
+            <List items={listItems} />
+            { renderButtons && (
+                <ButtonContainer>
+                    <SecondaryButton type='cancel' onClick={() => onClose()} />
+                    <PrimaryButton type='yes' onClick={() => confirm()}/>
+                </ButtonContainer>
+            )}
+        </Content>
+    );
+};
 
-export const showInfoPopup = (title, paragraphs, listItems, onClose) => {
+export const showInfoPopup = (title, paragraphs, listItems, onClose, onConfirm) => {
     const controls = showPopup(
         title,
-        getContent(paragraphs, listItems),
+        getContent(paragraphs, listItems, onClose, onConfirm),
         onClose,
         POPUP_OPTIONS
     );
