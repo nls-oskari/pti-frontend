3ace60b6e311d08563f410dd3c781ef2132db6fe
Olli Kauppinen <olli.kauppinen@nls.fi>
2025-10-23 09:43:39 +0300

    add test selectors

--- bundles/paikkatietoikkuna/coordinatetransformation/components/SrsSelect.jsx
+++ bundles/paikkatietoikkuna/coordinatetransformation/components/SrsSelect.jsx
@@ -7,9 +7,14 @@ import { ComponentLabel } from './ComponentLabel';
 import { BUNDLE, DATUM, SYSTEM, PROJECTION, SRS, SRS_H } from '../constants';
 import { getDimension } from '../helper';
 
-// Use EPSG code as title
-const SRS_OPTIONS = SRS.map(opt => ({ ...opt, title: opt.value }));
-const HEIGHT_OPTIONS = SRS_H.map(opt => ({ ...opt, title: opt.value }));
+// Use EPSG code as data-value (tests) and title
+const SRS_OPTIONS = SRS.map(opt => ({ ...opt, title: opt.value, 'data-value': opt.value }));
+const HEIGHT_OPTIONS = SRS_H.map(opt => ({ ...opt, title: opt.value, 'data-value': opt.value }));
+// data-value for tests
+const DATUM_OPTIONS = DATUM.map(opt => ({ ...opt, 'data-value': opt.value }));
+const SYSTEM_OPTIONS = SYSTEM.map(opt => ({ ...opt, 'data-value': opt.value }));
+const PROJECTION_OPTIONS = PROJECTION.map(opt => ({ ...opt, 'data-value': opt.value }));
+
 const Content = styled.div`
     display: flex;
     flex-direction: column;
@@ -22,11 +27,12 @@ const SelectWrapper = styled.div`
 `;
 const filter = (input, {label, value, replaced=''}) => `${label} ${value} ${replaced}`.toLowerCase().includes(input.toLowerCase());
 
-const Srs = ({ srs, options, onChange, controller, block = false }) => {
+const Srs = ({ srs, options, type, controller, block = false }) => {
     const [isOpen, setOpen] = useState(false);
     const placeholder = Oskari.getMsg(BUNDLE, `actions.${isOpen ? 'search': 'select'}`);
     // For some reason onOpenChange={open => setOpen(open)} doesn't work, use focus & blur
     return <LabeledSelect localize showSearch mandatory
+        idForTests={`${type}_srs`}
         block={block}
         onFocus={()=> setOpen(true)}
         onBlur={() => setOpen(false)}
@@ -37,7 +43,7 @@ const Srs = ({ srs, options, onChange, controller, block = false }) => {
         placeholder={placeholder}
         options={options}
         controller={controller}
-        onChange={onChange}/>
+        onChange={val => controller.setSrs(type, val)}/>
 };
 
 export const SrsSelect = ({ srs, heightSrs, type, minimal, controller }) => {
@@ -49,8 +55,8 @@ export const SrsSelect = ({ srs, heightSrs, type, minimal, controller }) => {
         return (
             <Content className={`t_srs_${type}`}>
                 <ComponentLabel label={`flyout.coordinateSystem.${type}.title`}/>
-                <Srs block srs={srs} options={SRS_OPTIONS} onChange={val => controller.setSrs(type, val)} controller={controller}/>
-                <LabeledSelect block label='flyout.coordinateSystem.heightSystem.label' value={heightSrs} placeholder={heightPH} disabled={heightDisabled} info='heightSystem' options={HEIGHT_OPTIONS} onChange={val => controller.setHeightSrs(type, val)} controller={controller}/>
+                <Srs block srs={srs} options={SRS_OPTIONS} type={type} controller={controller} />
+                <LabeledSelect block idForTests={`${type}_height`} label='flyout.coordinateSystem.heightSystem.label' value={heightSrs} placeholder={heightPH} disabled={heightDisabled} info='heightSystem' options={HEIGHT_OPTIONS} onChange={val => controller.setHeightSrs(type, val)} controller={controller}/>
             </Content>
         );
     }
@@ -71,8 +77,8 @@ export const SrsSelect = ({ srs, heightSrs, type, minimal, controller }) => {
         setDatum(datum);
     };
 
-    const systemOptions = datum ? SYSTEM.filter(opt => opt.datums.includes(datum)) : SYSTEM;
-    const projectionOptions = datum ? PROJECTION.filter(opt => opt.datums.includes(datum)) : PROJECTION;
+    const systemOptions = datum ? SYSTEM_OPTIONS.filter(opt => opt.datums.includes(datum)) : SYSTEM_OPTIONS;
+    const projectionOptions = datum ? PROJECTION_OPTIONS.filter(opt => opt.datums.includes(datum)) : PROJECTION_OPTIONS;
     const srsOptions = SRS_OPTIONS.filter(srs => {
         if (datum && datum !== srs.datum) {
             return false;
@@ -89,11 +95,11 @@ export const SrsSelect = ({ srs, heightSrs, type, minimal, controller }) => {
     return (
         <Content className={`t_srs_${type}`}>
             <ComponentLabel label={`flyout.coordinateSystem.${type}.title`}/>
-            <LabeledSelect label='flyout.coordinateSystem.geodeticDatum.label' info='geodeticDatum' value={datum} options={DATUM} onChange={onDatum} controller={controller}/>
-            <LabeledSelect localize label='flyout.coordinateSystem.coordinateSystem.label' info='coordinateSystem' value={system} options={systemOptions} onChange={onSystem} controller={controller}/>
-            { system === 'PROJ_2D' && <LabeledSelect label='flyout.coordinateSystem.mapProjection.label' info='mapProjection' value={projection} options={projectionOptions} onChange={setProjection} controller={controller}/> }
-            <Srs srs={srs} options={srsOptions} onChange={val => controller.setSrs(type, val)} controller={controller}/>
-            <LabeledSelect label='flyout.coordinateSystem.heightSystem.label' value={heightSrs} placeholder={heightPH} disabled={heightDisabled} info='heightSystem' options={HEIGHT_OPTIONS} onChange={val => controller.setHeightSrs(type, val)} controller={controller}/>
+            <LabeledSelect idForTests={`${type}_datum`} label='flyout.coordinateSystem.geodeticDatum.label' info='geodeticDatum' value={datum} options={DATUM_OPTIONS} onChange={onDatum} controller={controller}/>
+            <LabeledSelect idForTests={`${type}_system`} localize label='flyout.coordinateSystem.coordinateSystem.label' info='coordinateSystem' value={system} options={systemOptions} onChange={onSystem} controller={controller}/>
+            { system === 'PROJ_2D' && <LabeledSelect idForTests={`${type}_projection`} label='flyout.coordinateSystem.mapProjection.label' info='mapProjection' value={projection} options={projectionOptions} onChange={setProjection} controller={controller}/> }
+            <Srs srs={srs} options={srsOptions} type={type} controller={controller}/>
+            <LabeledSelect idForTests={`${type}_height`} label='flyout.coordinateSystem.heightSystem.label' value={heightSrs} placeholder={heightPH} disabled={heightDisabled} info='heightSystem' options={HEIGHT_OPTIONS} onChange={val => controller.setHeightSrs(type, val)} controller={controller}/>
         </Content>
     );
 };
