3cfc04904d897d48ffbbb1235c833315078f2987
Olli Kauppinen <olli.kauppinen@nls.fi>
2025-11-08 12:42:58 +0200

    add srs select for file import

--- bundles/paikkatietoikkuna/coordinatetransformation/components/FileSettings.jsx
+++ bundles/paikkatietoikkuna/coordinatetransformation/components/FileSettings.jsx
@@ -4,6 +4,7 @@ import { Message, Checkbox } from 'oskari-ui';
 import { FileInput } from 'oskari-ui/components/FileInput';
 import { LabeledSelect, LabeledInput } from './LabeledFields';
 import { FilePreview } from './FilePreview';
+import { ImportSrsSelect } from './SrsSelect';
 import { InfoIcon } from 'oskari-ui/components/icons';
 import { SEPARATORS, DECIMAL, DEGREE } from '../constants';
 import { isDegreeSystem } from '../helper';
@@ -51,17 +52,20 @@ const CheckboxOption = ({id, values, onChange, controller, locPath}) => (
     </Wrapper>
 );
 
-
 const SelectOption = ({id, values, onChange, controller, mandatory = true}) =>
     <LabeledSelect localize mandatory={mandatory} label={`fileSettings.options.${id}`} info={id} value={values[id]} options={OPTIONS[id]} onChange={value => onChange(id, value)} controller={controller}/>
 
-export const ImportFile = ({ import: values, inputSrs, files, fileContents, controller }) => {
+export const ImportFile = ({ import: values, inputSrs, inputHeightSrs, importSrs, importHeightSrs, files, fileContents, controller }) => {
     const onChange = (key, value) => controller.setFileSetting('import', key, value);
     // file parser handles count instead of boolean TODO: change to number input ?
     const onPrefix = (key, checked) => controller.setFileSetting('import', 'prefixColCount', checked ? 1 : 0);
+    // Note: handler.getImportDimension
+    const srs = importSrs === null ? inputSrs : importSrs;
+    const heightSrs = importHeightSrs === null ? inputHeightSrs : importHeightSrs;
     return (
         <Content>
             <FileInput mandatory onFiles={controller.setFiles} files={files} { ...FILE_INPUT_PROPS } />
+            <ImportSrsSelect type='import' srs={srs} heightSrs={heightSrs} controller={controller} />
             <LabeledInput info='headerLineCount' number min={0} label='fileSettings.options.headerLineCount' value={values.headerLineCount} onChange={value => onChange('headerLineCount', value)} controller={controller}/>
             <SelectOption id='coordinateSeparator' controller={controller} onChange={onChange} values={values}/>
             <SelectOption id='decimalSeparator' controller={controller} onChange={onChange} values={values}/>
